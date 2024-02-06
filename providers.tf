03bcbc19871877234ea6b3895585181fe7bea032
Javier Solares <89952745+jsolaresm@users.noreply.github.com>
2024-02-06 10:48:36 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=4.36.0, <4.47.0, !=4.43.0"
+      version = ">=4.36.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.4.3"
     }
   }
-  required_version = "~>1.3.0"
+  required_version = ">1.3.0"
 }
 
 provider "aws" {
